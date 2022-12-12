c7475c899c18e47f88243e8212261f8d2364e32d
Maher MATMATI <maher@sfeir136.fritz.box>
2022-12-12 05:19:08 +0100

    init

--- src/app/core/services/contries.service.ts
+++ src/app/core/services/contries.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
-import { Country } from "src/app/shared/models/contry.model";
+import { Country } from "../../shared/models/contry.model";
 
 @Injectable()
 export class CountryService {
